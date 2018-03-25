20720ce29320c0c6c45bc1df2128c4bb01820487
HugoVP <dev@hugo.plus>
2018-03-25 14:59:05 -0600

    Proyect structure refactoring

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,17 +3,27 @@ import React from 'react';
 import classes from './SideDrawer.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux/Aux';
 
 function sideDrawer(props) {
-    
+    let attachedClasses = `${classes.SideDrawer} ${props.open ? classes.Open : classes.Close}`;
 
     return (
-        <div className={classes.SideDrawer}>
-            <Logo />
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        <Aux>
+            <Backdrop
+                show={props.open}
+                clicked={props.closed}
+            />
+            <div className={attachedClasses}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
+            </div>
+        </Aux>
     );
 }
 
