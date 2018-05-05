f99aa8957072e4125721218b7118ce2d4c63fe4c
HugoVP <dev@hugo.plus>
2018-05-05 14:11:22 -0500

    Checkout container added

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -1,19 +1,36 @@
 import React from 'react';
 
+import classes from './CheckoutSummary.css'
+
 import Burger from '../../Burger/Burger';
+import Button from '../../UI/Button/Button';
 
 function checkoutSummary(props) {
   return (
-    <div>
+    <div className={classes.CheckoutSummary}>
       <h1>We hope it tastes well</h1>
       <div
         style={{
-          width: '300px',
+          width: '100%',
           height: '300px',
           margin: 'auto',
         }}
       >
-        <Burger />
+        <Burger ingredients={props.ingredients} />
+        
+        <Button
+          btnType="Danger"
+          clicked
+        >
+          CANCEL
+        </Button>
+
+        <Button
+          btnType="Success"
+          clicked
+        >
+          CONTINUE
+        </Button>
       </div>
     </div>
   )
--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 
+import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+
 class Checkout extends Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      meat: 1,
+      cheese: 1,
+      bacon: 1,
+    },
+  }
+  
   render() {
     return (
-      <div>Hello</div>
+      <div>
+        <CheckoutSummary ingredients={this.state.ingredients} />
+      </div>
     )
   }
 }
